ac45e1ad8d607842b59f730fc766a34fc31bdbcf
Maybe KAL <kal523721@gmail.com>
2026-07-19 15:53:28 +0300

    تعديل وإدارة المجلدات والمشاريع (عبر لوحة التحكم)

--- projects-data.js
+++ projects-data.js
@@ -2,13 +2,4 @@
 // يمكنك تعديل المشاريع القائمة، حذفها، أو إضافة مشاريع جديدة هنا بسهولة!
 // سيقوم الموقع بتحديث العناصر تلقائياً بناءً على هذه البيانات.
 
-const PROJECTS_DATA = [
-    {
-        "id": "GHERGEG",
-        "title": "GEGGSD",
-        "icon": "📁",
-        "description": "GEGEGEG",
-        "technologies": "EGGEG",
-        "files": []
-    }
-];
+const PROJECTS_DATA = [];
